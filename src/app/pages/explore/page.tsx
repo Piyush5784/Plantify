f852682df38a4fd457f3f9dcb2c44f612444e291
Piyush Kumar Jha <105655779+Piyush5784@users.noreply.github.com>
2024-10-14 15:47:55 +0530

    build success

--- src/app/pages/explore/page.tsx
+++ src/app/pages/explore/page.tsx
@@ -1,17 +1,4 @@
 "use client";
-<<<<<<< HEAD
-import { AllItems } from "@/assets/Items";
-import ExplorePageCart, {
-  PlantCard,
-} from "@/components/custom/Explore-page-cart";
-import Navbar from "@/components/custom/Navbar";
-import Footer from "@/components/custom/Footer";
-import { useSelector } from "react-redux";
-import { ItemsType } from "@/types/itemsType";
-import { RootState } from "@/store";
-import { Sidebar } from "@/components/custom/Sidebar";
-import Link from "next/link";
-=======
 import { PlantCard } from "@/components/custom/Explore-page-plant-card";
 import Footer from "@/components/custom/Footer";
 import Navbar from "@/components/custom/Navbar";
@@ -19,7 +6,6 @@ import { Sidebar } from "@/components/custom/Sidebar";
 import { RootState } from "@/store";
 import { ItemsType } from "@/types/itemsType";
 import { useSelector } from "react-redux";
->>>>>>> features
 
 export default function Home() {
   const items = useSelector(
@@ -35,13 +21,8 @@ export default function Home() {
           <Sidebar />
           <div className="w-4/5 flex flex-wrap gap-5 items-center justify-center pb-5 p-1">
             {items.length > 0 ? (
-<<<<<<< HEAD
-              items.map((I: ItemsType) => (
-                <div key={I.id}>
-=======
               items.map((I: ItemsType, index: number) => (
                 <div key={index}>
->>>>>>> features
                   <PlantCard plant={I} />
                 </div>
               ))
